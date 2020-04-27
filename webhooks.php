34df0f40bf419f45d54a4b1cdba0d36aff5da4f0
numchai <41096000+numchai@users.noreply.github.com>
2020-04-27 16:43:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			
 			// Get text sent
-			$text = $event['source'];
+			$text = $event;
 			//$text = $event['source']['userId'];
 			
 			// Get replyToken
